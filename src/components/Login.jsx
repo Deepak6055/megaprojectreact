c0c4a1d317f3d788cda82ce6a15d156fdaa3a2bc
Deepak6055 <deepakrama321@gmail.com>
2024-06-12 16:16:00 +0530

    loginpage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -61,8 +61,19 @@ function Login() {
                                     "Email address must be a valid address",
                                 }
                             })}
-            
                             />
+                            <Input
+                            label="Password: "
+                            type="password"
+                            placeholder="Enter your password"
+                            {...register("password",{
+                                required:true
+                            })}
+                            />
+                            <Button type="submit"
+                            className= "w-full">
+                                Sign in 
+                            </Button>
                     </div>
                 </form>
         </h2>
