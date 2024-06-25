95e390dc978b516b6d5ad369c016b5729628f514
Deepak6055 <deepakrama321@gmail.com>
2024-06-25 12:20:14 +0530

    completion of authlayout

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -14,7 +14,10 @@ function Select({
       <select
       {...props}
       id = {id}
-      className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${classname}`}>
+      className= {`px-3 py-2 rounded-lg 
+      bg-white text-black outline-none 
+      focus:bg-gray-50 duration-200 border
+       border-gray-200 w-full ${classname}`}>
         {options?.map((option)=>{
             <option key={option} value={option}>
                 {option}
