2b022e2991cc38d1fc1f5ba8a5a79ec781dd71c0
Deepak6055 <deepakrama321@gmail.com>
2024-06-26 15:52:26 +0530

    now its ready for debugging

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,25 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE"
+import Signup from "./Signup";
+import Login from "../components/Login"
+import PostCard from "./PostCard"
+import PostForm from "./post-form/PostForm"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
 export  {
     Header,
     Container,
     Logo,
     Footer,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input
 }
